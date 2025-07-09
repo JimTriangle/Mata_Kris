943c57ee8da0a102725b88cc8d582ab105063e15
JimTriangle <jmariepujade@gmail.com>
2025-07-09 14:46:21 +0200

    correction du surlignage menu

--- resources/views/components/layouts/partials/header.blade.php
+++ resources/views/components/layouts/partials/header.blade.php
@@ -3,10 +3,10 @@
         <h1><a href="{{ route('accueil') }}">Mata & Kris</a></h1>
         <p class="subtitle">Les Feuilles Chantantes</p>
         <nav>
-            <a href="{{ route('accueil') }}">Accueil</a>
-            <a href="{{ route('concerts') }}">Concerts</a>
-            <a href="{{ route('galerie') }}">Galerie</a>
-            <a href="{{ route('contact') }}">Contact</a>
+            <a href="{{ route('accueil') }}" @class(['active' => request()->routeIs('accueil')])>Accueil</a>
+            <a href="{{ route('concerts') }}" @class(['active' => request()->routeIs('concerts')])>Concerts</a>
+            <a href="{{ route('galerie') }}" @class(['active' => request()->routeIs('galerie')])>Galerie</a>
+            <a href="{{ route('contact') }}" @class(['active' => request()->routeIs('contact')])>Contact</a>
         </nav>
     </div>
 </header>
